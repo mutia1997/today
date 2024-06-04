4695d8406b3557731fa945966295b1eec68c0655
mutia1997 <158068300+mutia1997@users.noreply.github.com>
2024-06-04 22:03:10 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,46 +40,42 @@ fig_fl.update_layout(plot_bgcolor='#ffffe0')  # Soft background color (krem)
 fig_fl.update_traces(line=dict(color='#00008b'))  # Soft line color (warna biru tua)
 st.plotly_chart(fig_fl, use_container_width=True)
 
-# Display Top 5 Financial Losses table by Date with Blue Border
+# Display Top 5 Financial Losses table by Date
 st.subheader('Top 5 Financial Losses by Date')
 top_5_fl_tanggal = fl_tanggal_data.head(5).reset_index(drop=True)
 top_5_fl_tanggal.index += 1  # Start index from 1
-st.write(top_5_fl_tanggal, unsafe_allow_html=True)
+st.table(top_5_fl_tanggal)
 
 # Display option for more details on Top 5 Financial Losses table by Date
 if st.button('Click here for more details (by Date)', key='fl_tanggal_detail'):
-    fl_tanggal_data.index += 1
-    st.write(fl_tanggal_data)
+    st.table(fl_tanggal_data)
 
-# Display Top 5 Lost Products table with Blue Border
+# Display Top 5 Lost Products table
 st.subheader('Top 5 Lost Products')
 top_5_lost_products = lost_products_data.head(5).reset_index(drop=True)
 top_5_lost_products.index += 1
-st.write(top_5_lost_products, unsafe_allow_html=True)
+st.table(top_5_lost_products)
 
 # Display option for more details on Top 5 Lost Products table
 if st.button('Click here for more details', key='lost_products_detail'):
-    lost_products_data.index += 1
-    st.write(lost_products_data)
+    st.table(lost_products_data)
 
-# Display Top 5 Financial Losses table by Products with Blue Border
+# Display Top 5 Financial Losses table by Products
 st.subheader('Top 5 Financial Losses by Products')
 top_5_fl_produk = fl_produk_data.head(5).reset_index(drop=True)
 top_5_fl_produk.index += 1  # Start index from 1
-st.write(top_5_fl_produk, unsafe_allow_html=True)
+st.table(top_5_fl_produk)
 
 # Display option for more details on Top 5 Financial Losses table by Products
 if st.button('Click here for more details (by Products)', key='fl_produk_detail'):
-    fl_produk_data.index += 1
-    st.write(fl_produk_data)
+    st.table(fl_produk_data)
 
-# Display Top 5 Financial Losses table by Machine with Blue Border
+# Display Top 5 Financial Losses table by Machine
 st.subheader('Top 5 Financial Losses by Machine')
 top_5_fl_mesin = fl_mesin_data.head(5).reset_index(drop=True)
 top_5_fl_mesin.index += 1  # Start index from 1
-st.write(top_5_fl_mesin, unsafe_allow_html=True)
+st.table(top_5_fl_mesin)
 
 # Display option for more details on Top 5 Financial Losses table by Machine
 if st.button('Click here for more details (by Machine)', key='fl_mesin_detail'):
-    fl_mesin_data.index += 1
-    st.write(fl_mesin_data)
+    st.table(fl_mesin_data)
